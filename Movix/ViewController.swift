8a096fca0d3f48a4511e51e0d9a3bda34893ecee
Enrique Sanchez <enrique.sanchezisc@gmail.com>
2024-07-26 19:57:26 -0600

    Creación del proyecto en xcode

---
+++ Movix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movix
+//
+//  Created by Enrique Sanz on 26/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
